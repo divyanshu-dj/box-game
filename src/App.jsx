cccf278746265b6ea8419cd5865484c8829d8367
divyanshu <jhadivyanshu042@gmail.com>
2024-03-23 16:06:05 +0530

    fixed the keyboard and cursor integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,25 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [pressedKey, setPressedKey] = useState(null);
-  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.key.startsWith("Arrow")) {
         setPressedKey(e.key);
-        console.log(pressedKey);
+      }
+    };
+    const handleKeyUp = (e) => {
+      if (e.key.startsWith("Arrow")) {
+        setPressedKey(null);
       }
     };
     document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
     };
-  }, [pressedKey]);
-
-  const handleClick = () => {
-    setIsClicked(true);
-  };
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen p-5 bg-red-500">
@@ -31,33 +32,29 @@ function App() {
       <div>
         <div className="arrow-buttons grid grid-cols-3 grid-rows-3 gap-2">
           <button
-            onClick={handleClick}
-            className={`col-end-3 row-start-1 h-20 w-20 border-2 rounded-md border-black bg-slate-300 hover:bg-yellow-300 transition-transform duration-300 ${
-              isClicked || pressedKey === "ArrowUp" ? "active:scale-75" : ""
+            className={`col-end-3 row-start-1 h-20 w-20 border-2 rounded-md border-black bg-slate-300  transition-transform duration-300 active:scale-75 active:bg-yellow-300 ${
+              pressedKey === "ArrowUp" ? "scale-75 bg-yellow-300" : ""
             }`}
           >
             &uarr;
           </button>
           <button
-            onClick={handleClick}
-            className={`col-end-2 row-start-2 h-20 w-20 border-2 rounded-md border-black bg-slate-300 hover:bg-yellow-300 transition-transform duration-300 ${
-              isClicked || pressedKey === "ArrowLeft" ? "active:scale-75" : ""
+            className={`col-end-2 row-start-2 h-20 w-20 border-2 rounded-md border-black bg-slate-300  transition-transform duration-300 active:scale-75 active:bg-yellow-300 ${
+              pressedKey === "ArrowLeft" ? "scale-75 bg-yellow-300" : ""
             }`}
           >
             &larr;
           </button>
           <button
-            onClick={handleClick}
-            className={`col-end-4 row-start-2 h-20 w-20 border-2 rounded-md border-black bg-slate-300 hover:bg-yellow-300 transition-transform duration-300  ${
-              isClicked || pressedKey === "ArrowRight" ? "active:scale-75" : ""
+            className={`col-end-4 row-start-2 h-20 w-20 border-2 rounded-md border-black bg-slate-300  transition-transform duration-300 active:scale-75 active:bg-yellow-300  ${
+              pressedKey === "ArrowRight" ? "scale-75 bg-yellow-300" : ""
             }`}
           >
             &rarr;
           </button>
           <button
-            onClick={handleClick}
-            className={`col-end-3 row-start-3 h-20 w-20 border-2 rounded-md border-black bg-slate-300 hover:bg-yellow-300 transition-transform duration-300  ${
-              isClicked || pressedKey === "ArrowDown" ? "active:scale-75" : ""
+            className={`col-end-3 row-start-3 h-20 w-20 border-2 rounded-md border-black bg-slate-300  transition-transform duration-300 active:scale-75 active:bg-yellow-300  ${
+              pressedKey === "ArrowDown" ? "scale-75 bg-yellow-300" : ""
             }`}
           >
             &darr;
