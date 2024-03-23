87dfd2db49e4d89c4175d99b720e8bfeb33f32f0
divyanshu <jhadivyanshu042@gmail.com>
2024-03-23 21:35:11 +0530

    done task

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ function App() {
   const [circlePos, setCirclePos] = useState({ x: 0, y: 0 });
   const [success, setSuccess] = useState(false);
   const [pressedKey, setPressedKey] = useState(null);
-  const outerDivRef = useRef(null);
-  const innerDivRef = useRef(null);
+  
 
   useEffect(() => {
     const handleKeyDown = (e) => {
@@ -57,27 +56,36 @@ function App() {
 
 
   useEffect(() => {
-    const generateRandomNumber = (min, max) => {
-      const randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
+    const generateRandomNumber = (min, max, excludedMin, excludedMax) => {
+      let randomValue;
+      do {
+        randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
+      } while (randomValue >= excludedMin && randomValue <= excludedMax);
       return randomValue;
     };
 
-    const newY = generateRandomNumber(0,410);
-    const newX = generateRandomNumber(0, 970);
+    const newY = generateRandomNumber(0,410, 20, 170);
+    const newX = generateRandomNumber(0, 970, 800, 950);
 
     setCirclePos({x:newX, y:newY});
   }, []);
+
+  useEffect(() => {
+    circlePos.x > 800+20 && circlePos.x < 950-10 && circlePos.y > 20+4 && circlePos.y < 170-25 ? setSuccess(true) : setSuccess(false);
+  },[circlePos])
 
   
 
   return (
     <div className="flex flex-col items-center justify-center h-screen p-5 bg-red-500">
-      <div ref={outerDivRef} className="playground relative w-[1000px] h-[500px] bg-green-300 m-5 border-8 rounded-md border-black">
-        <div ref={innerDivRef} className="target-box mt-5 border-4 border-blue-400 border-dashed absolute h-[150px] w-[150px] right-5"></div>
+      
+      <div  className="playground relative w-[1000px] h-[500px] bg-green-300 m-5 border-8 rounded-md border-black">
+        <div  className={`target-box border-4 border-blue-400 border-dashed absolute h-[150px] w-[150px] top-[20px] right-[20px] ${success ? 'bg-red-700': ''}`}></div>
         <div
           className="circle h-[25px] w-[25px] rounded-full bg-green-700 transition-transform duration-200 ease-in-out absolute"
           style={{ transform: `translate(${circlePos.x}px, ${circlePos.y}px)` }}
         ></div>
+        <div className="absolute bottom-5 right-[450px] ">{success ? <p className="font-bold">YOU WON!!</p> : <p></p>}</div>
       </div>
 
       <div>
@@ -125,8 +133,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
